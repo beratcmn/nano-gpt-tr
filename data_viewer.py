50bbf201e3e6af7426edb4661f6113ef782f09b0
Berat Çimen <beratcmn@hotmail.com>
2023-11-02 13:42:53 +0300

    Added vocabsize

--- data_viewer.py
+++ data_viewer.py
@@ -1,7 +1,12 @@
 def main():
-    with open('data.md', 'r') as f:
+    with open('data.md', 'r', encoding="UTF-8") as f:
         data = f.read()
-        print(len(data))
+        print(f"Length of text: {len(data)}")
+
+        chars = sorted(list(set(data)))
+        vocab_size = len(chars)
+        print(f"Number of unique characters: {vocab_size}")
+        print(chars)
 
 
 if __name__ == '__main__':
